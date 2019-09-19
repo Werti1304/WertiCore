7cbf5d709c0799a7f36953e03e99aedd716435cb
David Noah Peinsipp <werti1304@gmail.com>
2019-09-19 22:32:34 +0200

    Minor CorePlayer Extension

--- src/main/java/com/werti/plugins/core/CorePlayer.java
+++ src/main/java/com/werti/plugins/core/CorePlayer.java
@@ -1,6 +1,7 @@
 package com.werti.plugins.core;
 
 import jdk.internal.jline.internal.Nullable;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
@@ -93,5 +94,39 @@ public class CorePlayer
   public Player getPlayer()
   {
     return player;
+  }
+
+  ////////////////////////////////////////
+  // Implementation of Player-Functions //
+  ////////////////////////////////////////
+
+  /**
+   * Teleports a CorePlayer to another CorePlayer.
+   *
+   * @param corePlayer CorePlayer to be teleported to.
+   */
+  public void teleport(CorePlayer corePlayer)
+  {
+    player.teleport(corePlayer.getPlayer());
+  }
+
+  /**
+   * Send a message to a CorePlayer.
+   *
+   * @param string Message to be sent.
+   */
+  public void sendMessage(String string)
+  {
+    player.sendMessage(string);
+  }
+
+  /**
+   * Get the location of a CorePlayer.
+   *
+   * @return Location of the player.
+   */
+  public Location getLocation()
+  {
+    return player.getLocation();
   }
 }
