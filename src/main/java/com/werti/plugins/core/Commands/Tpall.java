3f07d0578af528f86e0a204d48063317ff69451e
Lukas Adlboller <lukasadlboller@gmail.com>
2019-09-26 19:31:51 +0200

    Implementing /tpall command
    / tpall Adding functionality

--- src/main/java/com/werti/plugins/core/Commands/Tpall.java
+++ src/main/java/com/werti/plugins/core/Commands/Tpall.java
@@ -1,5 +1,8 @@
 package com.werti.plugins.core.Commands;
 
+import com.werti.plugins.core.CorePlayer;
+import jdk.internal.jline.internal.Nullable;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,7 +11,7 @@ import org.bukkit.entity.Player;
 public class Tpall implements CommandExecutor
 {
   @Override
-  public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings)
+  public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args)
   {
     if (!(commandSender instanceof Player))
     {
@@ -16,9 +19,68 @@ public class Tpall implements CommandExecutor
       return true;
     }
 
-    // TODO: Lukas add functionality
-    commandSender.sendMessage("Lol");
+    Location tpToLocation = null;
 
-    return false;
+    if(args.length == 0)
+    {
+      tpToLocation = ((Player) commandSender).getLocation();
+    }
+    else
+    {
+      switch(args.length)
+      {
+        case 1:
+          CorePlayer tmpPlayer = CorePlayer.GetByPlayerName(args[0]);
+          if(tmpPlayer == null)
+          {
+            return false;
+          }
+          tpToLocation = tmpPlayer.getLocation();
+          break;
+        case 3:
+          tpToLocation = parseLocationFromStrings(commandSender, args[0], args[1], args[2]);
+          break;
+        default:
+          return false;
+      }
+    }
+
+    if(tpToLocation == null)
+    {
+      return false;
+    }
+
+    for(CorePlayer cPlayer : CorePlayer.getCorePlayerMap().values())
+    {
+      if(cPlayer.getPlayer().getWorld() == ((Player) commandSender).getWorld())
+      {
+        cPlayer.getPlayer().teleport(tpToLocation);
+        cPlayer.sendMessage(((Player) commandSender).getDisplayName() + " teleported you to this loaction!");
+      }
+    }
+
+    commandSender.sendMessage("All players in your world were teleported to this location!");
+    return true;
+  }
+
+  @Nullable
+  public Location parseLocationFromStrings(CommandSender commandSender, String strX, String strY, String strZ)
+  {
+    double tmpX;
+    double tmpY;
+    double tmpZ;
+
+    try
+    {
+      tmpX = Double.parseDouble(strX);
+      tmpY = Double.parseDouble(strY);
+      tmpZ = Double.parseDouble(strZ);
+    }
+    catch(NumberFormatException ex)
+    {
+      return null;
+    }
+
+    return new Location (((Player)commandSender).getWorld(), tmpX, tmpY, tmpZ);
   }
 }
